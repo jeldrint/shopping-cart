c5cb2617306297eef3d0241dcc8f77275f4524e2
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2023-05-10 06:45:10 +0800

    initial

---
+++ src/components/CandyStore.js
@@ -0,0 +1,16 @@
+import React, { useCallback } from "react";
+import RenderItem from "./RenderItem";
+
+const CandyStore = () => {
+
+
+    
+    return (
+        <>
+            <h1>Candy Store</h1>
+            <RenderItem />
+        </>
+    )
+}
+
+export default CandyStore
---
+++ src/components/RenderItem.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+const RenderItem = () => {
+    
+    return (
+        <h1>RenderItem</h1>
+    )
+}
+
+export default RenderItem
