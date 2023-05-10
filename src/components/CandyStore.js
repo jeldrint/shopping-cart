e393e06b1ff0d65dadf8289c8487f33d27c647e0
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2023-05-11 04:09:36 +0800

    partial push (Windows)

--- src/components/CandyStore.js
+++ src/components/CandyStore.js
@@ -1,13 +1,18 @@
 import React from "react";
 import RenderItem from "./RenderItem";
+import Cart from '../images/cart.png'
 
 const CandyStore = () => {
 
-
-    
     return (
         <>
-            <h1>Candy Store</h1>
+            <div className="store-header">
+                <h1 style={{margin: 0}}>Candies be love</h1>
+                <div className="cart">
+                    <img src ={Cart} className="cart-logo"/>
+                    <span className="cart-notif">0</span>
+                </div>
+            </div>
             <div className="store-content">
                 <RenderItem />
             </div>
